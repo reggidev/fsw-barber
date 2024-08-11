2566ead8752fb3ae669ef75cd583dd35158aabde
reggidev <reggidev@gmail.com>
2024-08-11 19:42:05 -0300

    feat: ensure only signed in user can book

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -6,14 +6,13 @@ import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/search"
 import Link from "next/link"
 import Image from "next/image"
-import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "./ui/dialog"
-import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog"
-import { signIn, signOut, useSession } from "next-auth/react"
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog"
+import { signOut, useSession } from "next-auth/react"
 import { Avatar, AvatarImage } from "./ui/avatar"
+import SignInDialog from "./sign-in-dialog"
 
 const SidebarSheet = () => {
   const { data } = useSession()
-  const handleLoginWithGoogleClick = () => signIn("google")
   const handleLogoutClick = () => signOut()
 
   return (
@@ -44,26 +43,7 @@ const SidebarSheet = () => {
                 </Button>
               </DialogTrigger>
               <DialogContent className="w-[90%]">
-                <DialogHeader>
-                  <DialogTitle>Faça login na plataforma</DialogTitle>
-                  <DialogDescription>
-                    Conecte-se usando sua conta do Google
-                  </DialogDescription>
-                </DialogHeader>
-
-                <Button
-                  variant="outline"
-                  className="gap-1 font-bold"
-                  onClick={handleLoginWithGoogleClick}
-                >
-                  <Image
-                    alt="Fazer login com o Google"
-                    src="/google.svg"
-                    width={18}
-                    height={18}
-                  />
-                  Google
-                </Button>
+                <SignInDialog />
               </DialogContent>
             </Dialog>
           </>
---
+++ app/_components/sign-in-dialog.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+import { DialogDescription, DialogHeader, DialogTitle } from "./ui/dialog"
+import { Button } from "./ui/button"
+import Image from "next/image"
+import { signIn } from "next-auth/react"
+
+const SignInDialog = () => {
+  const handleLoginWithGoogleClick = () => signIn("google")
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>Faça login na plataforma</DialogTitle>
+        <DialogDescription>
+          Conecte-se usando sua conta do Google
+        </DialogDescription>
+      </DialogHeader>
+
+      <Button
+        variant="outline"
+        className="gap-1 font-bold"
+        onClick={handleLoginWithGoogleClick}
+      >
+        <Image
+          alt="Fazer login com o Google"
+          src="/google.svg"
+          width={18}
+          height={18}
+        />
+        Google
+      </Button>
+    </>
+  )
+}
+
+export default SignInDialog
