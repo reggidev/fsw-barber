dde9fe5b68e0a280d0f17c812112aedae2f994f2
reggidev <reggidev@gmail.com>
2024-12-11 21:55:37 -0300

    feat: ensure footer date is always correct

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,5 +1,6 @@
-import React from "react"
-import { Card, CardContent } from "./ui/card"
+import React from 'react'
+
+import { Card, CardContent } from './ui/card'
 
 const Footer = () => {
   return (
@@ -7,7 +8,8 @@ const Footer = () => {
       <Card>
         <CardContent className="px-5 py-6">
           <p className="text-sm text-gray-400">
-            &copy; 2023 Copyright <span className="font-bold">FSW Barber</span>
+            &copy; {new Date().getFullYear()} Copyright{' '}
+            <span className="font-bold">FSW Barber</span>
           </p>
         </CardContent>
       </Card>
