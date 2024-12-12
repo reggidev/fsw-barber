91a916fea79e54371450b52765ec4b3685429e73
reggidev <reggidev@gmail.com>
2024-12-11 22:41:10 -0300

    feat: ensure barbershops are displayed correctly to the user

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -39,23 +39,25 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
   })
 
   return (
-    <div>
+    <>
       <Header />
-      <div className="my-6 px-5">
-        <Search />
-      </div>
-      <div className="px-5">
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Resultados para &quot;{searchParams.title || searchParams?.service}
-          &quot;
-        </h2>
-        <div className="grid grid-cols-2 gap-4">
-          {barbershops.map((barbershop) => (
-            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-          ))}
+      <div className="mx-auto md:max-w-[1625px]">
+        <div className="my-6 px-5">
+          <Search />
+        </div>
+        <div className="px-5">
+          <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+            Resultados para &quot;{searchParams.title || searchParams?.service}
+            &quot;
+          </h2>
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-6">
+            {barbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
