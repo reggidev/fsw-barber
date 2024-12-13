471c28ab467b7864d164f4a3b2c18cd6c5ef2759
reggidev <reggidev@gmail.com>
2024-12-13 15:43:05 -0300

    feat: ensure barbershop page are responsible

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,13 +1,12 @@
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from 'lucide-react'
+import { ChevronLeftIcon, MapPinIcon, StarIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
 
+import Header from '@/app/_components/header'
 import PhoneItem from '@/app/_components/phone-item'
 import ServiceItem from '@/app/_components/service-item'
-import SidebarSheet from '@/app/_components/sidebar-sheet'
 import { Button } from '@/app/_components/ui/button'
-import { Sheet, SheetTrigger } from '@/app/_components/ui/sheet'
 import { db } from '@/app/_lib/prisma'
 
 interface BarbershopPageProps {
@@ -31,81 +30,82 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
   }
 
   return (
-    <div>
-      {/* IMAGEM */}
-      <div className="relative h-[250px] w-full">
-        <Image
-          alt={barbershop?.name}
-          src={barbershop?.imageUrl}
-          fill
-          className="object-cover"
-        />
+    <>
+      <Header />
+      <div className="mx-auto max-w-[1400px]">
+        {/* IMAGEM */}
+        <div className="relative h-[250px] w-full md:hidden">
+          <Image
+            alt={barbershop?.name}
+            src={barbershop?.imageUrl}
+            fill
+            className="object-cover"
+          />
 
-        <Button
-          size="icon"
-          variant="secondary"
-          className="absolute left-4 top-4"
-          asChild
-        >
-          <Link href="/">
-            <ChevronLeftIcon />
-          </Link>
-        </Button>
+          <Button
+            size="icon"
+            variant="secondary"
+            className="absolute left-4 top-4"
+            asChild
+          >
+            <Link href="/">
+              <ChevronLeftIcon />
+            </Link>
+          </Button>
+        </div>
 
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button
-              size="icon"
-              variant="outline"
-              className="absolute right-4 top-4"
-            >
-              <MenuIcon />
+        {/* TITULO */}
+        <div className="border-b border-solid p-5">
+          <div className="mb-3 hidden md:flex">
+            <Button size="icon" variant="secondary" asChild>
+              <Link href="/">
+                <ChevronLeftIcon />
+              </Link>
             </Button>
-          </SheetTrigger>
-          <SidebarSheet />
-        </Sheet>
-      </div>
-
-      {/* TITULO */}
-      <div className="border-b border-solid p-5">
-        <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
-        <div className="mb-2 flex items-center gap-2">
-          <MapPinIcon className="text-primary" size={18} />
-          <p className="text-sm">{barbershop?.address}</p>
+          </div>
+          <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
+          <div className="mb-2 flex items-center gap-2">
+            <MapPinIcon className="text-primary" size={18} />
+            <p className="text-sm">{barbershop?.address}</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <StarIcon className="fill-primary text-primary" size={18} />
+            <p className="text-sm">5,00 (499 avaliações)</p>
+          </div>
         </div>
-        <div className="flex items-center gap-2">
-          <StarIcon className="fill-primary text-primary" size={18} />
-          <p className="text-sm">5,00 (499 avaliações)</p>
+
+        {/* DESCRIÇÃO */}
+        <div className="space-y-3 border-b border-solid p-5">
+          <h2 className="text-xs font-bold uppercase text-gray-400">
+            Sobre nós
+          </h2>
+          <p className="text-justify text-sm">{barbershop?.description}</p>
         </div>
-      </div>
 
-      {/* DESCRIÇÃO */}
-      <div className="space-y-3 border-b border-solid p-5">
-        <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
-        <p className="text-justify text-sm">{barbershop?.description}</p>
-      </div>
+        {/* SERVIÇOS */}
+        <div className="space-y-3 border-b border-solid p-5">
+          <h2 className="text-xs font-bold uppercase text-gray-400">
+            Serviços
+          </h2>
+          <div className="mx-auto grid gap-y-3 md:max-w-screen-md md:grid-cols-2 md:gap-x-3 lg:max-w-screen-lg lg:grid-cols-3 2xl:max-w-screen-2xl 2xl:grid-cols-4">
+            {barbershop.services.map((service) => (
+              <ServiceItem
+                key={service.id}
+                barbershop={JSON.parse(JSON.stringify(barbershop))}
+                service={JSON.parse(JSON.stringify(service))}
+              />
+            ))}
+          </div>
+        </div>
 
-      {/* SERVIÇOS */}
-      <div className="space-y-3 border-b border-solid p-5">
-        <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
-        <div className="space-y-3">
-          {barbershop.services.map((service) => (
-            <ServiceItem
-              key={service.id}
-              barbershop={JSON.parse(JSON.stringify(barbershop))}
-              service={JSON.parse(JSON.stringify(service))}
-            />
+        {/* CONTATO */}
+        <div className="mx-auto max-w-[400px] space-y-3 p-5">
+          {barbershop.phones.map((phone) => (
+            <PhoneItem key={phone} phone={phone} />
           ))}
         </div>
       </div>
-
-      {/* CONTATO */}
-      <div className="space-y-3 p-5">
-        {barbershop.phones.map((phone) => (
-          <PhoneItem key={phone} phone={phone} />
-        ))}
-      </div>
-    </div>
+    </>
   )
 }
 
